579f568e1a981eb6afb05425adabb5c10848cf3b
gabriel pettier <dev@tangibledisplay.com>
2016-08-20 15:39:06 +0200

    make example runnable from anywhere

--- example.py
+++ example.py
@@ -1,6 +1,6 @@
 from kivy.base  import  runTouchApp
 from kivy.lang  import  Builder
-from knob       import  Knob
+from kivy.garden.knob import  Knob
 
 # LOAD KV UIX
 runTouchApp(Builder.load_file('example.kv'))
